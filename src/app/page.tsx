ef765abcc24f92e53d154880173d32f2349504dc
RuthvikSR <theruthviksr@gmail.com>
2025-08-19 17:29:26 +0530

    [FEAT] : adds youtube logo to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import {Button} from "@/components/ui/button"
+import Image from "next/image"
 
 export default function Home() {
   return(
     <div>
-      <p className = "font-bold text-rose-500">
-        Hello World!
-      </p>
-      <Button variant = "destructive">
-        Click Me
-      </Button>
+      <Image src = "/logo.svg" height = {50} width = {50} alt = "Logo">
+      </Image>
     </div>
   )
 }
